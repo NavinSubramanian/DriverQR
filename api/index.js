53d32a1b0d7d3447dd7bb8b13a53e68d0fc92c06
Navin Subramanian <assasin71003@gmail.com>
2024-03-07 13:51:10 +0530

    api test v5

--- api/index.js
+++ api/index.js
@@ -134,19 +134,24 @@ app.put('/users/:id', async (req, res) => {
 
     // Find the user by ID and update the fields
     const user = await User.findById(id)
-    console.log(user.userDetails)
-    let dEntries = Object.entries(user.userDetails)
-    let d2Entries = Object.entries(updatedFields)
+    let dEntries = user.userDetails
+    let d2Entries = updatedFields
 
-    for (let i = 0; i < dEntries.length; i++) {
-        let [k, v] = dEntries[i];
-        let [k2, v2] = d2Entries[i];
-        
-        if (v !== v2){
-          const up = await User.findByIdAndUpdate(id,{$set:{k:v2}},{new:true})
-          console.log(up)
-        }
-    }
+    user_b: [
+      personName,
+      emergencyNumber,
+      gender,
+      phoneNumber,
+      address,
+      bloodGroup,
+    ]
+
+    user_b.map((key)=>{
+      if(dEntries[key] != d2Entries[key]){
+        const up = User.findByIdAndUpdate(id,{$set:{key:d2Entries[key]}},{new:true})
+        console.log(up)
+      }
+    })
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
